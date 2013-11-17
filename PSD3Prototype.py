9d25fce0d939e6ed36567bb9a924ada0a30f7e87
valeyard <mrpontius@gmail.com>
2013-11-17 22:25:57 +0000

    removed things, cleaned up, less hardcode
    I removed things which I was sure werent being used
    Changed it so the user has the option to state what kind of file they
    are loading/saving

--- PSD3Prototype.py
+++ PSD3Prototype.py
@@ -11,84 +11,12 @@ import string
 ##
 ###############################################################################################################################
 ###############################################################################################################################
-##
-## This program is to manage a data base which stores the best personal times of
-## the members of a certain swimming club.
-##
-## The program loads the data from a file stored on the disk and place those data
-## into a dictionary stored in memory.
-## The program also allows the user to print the results ordered by time and
-## grouped by styles and gender.
-## The user can also update the database and save the updated results into a data
-## file stored on the disk.
-##
-##
-## Data structures:
-## records type<dict>:
-##      Its structure is {ID:{'event':event,'gender':gender,'age':age','time':time,'name':name'}
-##
-##      As it can be seen is a dictionary which contains a subdictionary which
-##      contains the data for each case.
-##
-##      ID: is the main key. This is to eventualy manage the problem that arises
-##          when there are two different swimmers with the same name compiting in
-##          the same event. This is not implemented in the actual code, but leaves
-##          the code ready to do so.
-##
-##      {subdictionary}:
-##          event:  type<str>   stores the event in which the swimmer got their personal best.
-##          gender: type<str>   stores swimmer's gender.
-##          age:    type<int>   stores swimmer's age.
-##          time:   type<float> stores swimmer's personal best in float point format.
-##          name:   type<str>   stores swimmer's name.
-##
-## timeList type<list>:
-##      This is a list generated within the sortByTime(records) function. It is
-##      an auxiliary data structure to use the sort method implemented in python.
-##
-##
-## Data File format:
-## The data are stored on a text file in this format:
-##
-##      Event:Gender:Age:Time:Name
-##
-## so, the layout of the file is:
-##
-##      IM:F:6:58.2:Kirsty Laing
-##      BA:F:6:43.5:Joan Pine
-##      IM:M:6:57.2:Eric Idle
-##      IM:M:6:58.2:Ciaran Johnson
-##
-##
-## Limitations and exceptions:
-##
-## The program is able to handle exceptions such as not valid filenames when loading
-## or saving the data files. It also handles bad user entries on the menu or when
-## different options are offered to the user (see, e.g., 'chooseEvent' or 'another'
-## functions). However, in the case of the updateTimes() function it is assumed that
-## the user types the time in the right format.
-##
-## The sorting method employed to sort the results in function of the time is the
-## own sorting algorithm implemented in the python method for lists list.sort().
-## This is probably not the best, but because of the restrictions of the exam
-## conditions it is deemed as the best solution in this context.
-##
-## In this sense, the routine to check and update the results is not the best either.
-## However, for the size of the database involved in this case, it is reliable enough.
-##
-###############################################################################################################################
-###############################################################################################################################
-
 
 records = {}                # global dictionary to store the student records loaded from the disk.
 courses = {}                # global dictionary to store the courses information loaded from the disk.
 quitProgram = False         # boolean variable to control the execution of the program.
 debug = 1                   # debug control. 1 sets the debug mode ON, 0 sets the debug mode OFF.
 
-
-###############################################################################################################################
-###############################################################################################################################
-##
 ## another(): auxiliary function to manage the retry situations within the different main functions.
 def another():
     options = ('y','n')                             # valid user options.
@@ -241,9 +169,6 @@ def loadFile(fileType):
         print fileName,'not found'                  # message to the user.
     return                                          # exits gently
 
-
-
-
 ## saveFile(fileType, dataStructure): this function places the data stored in memory into a text file stored on disk.
 def saveFile(fileType, dataStructure):
     nameError = True                                # exception flag.
@@ -265,104 +190,6 @@ def saveFile(fileType, dataStructure):
                 saveCoursesFile(fileName, dataStructure)
     return
 
-
-###############################################################################################################################
-###############################################################################################################################
-##
-## Print functions
-##
-###############################################################################################################################
-###############################################################################################################################
-##
-## sortByTime(records) makes a copy of the data stored in records on a list and sort the records by time
-def sortByTime(records):
-    timeList = []                               # creates the list.
-
-    for ID in records:                          # makes the copy onto the list.
-        timeList = timeList + [[records[ID]['time'],records[ID]['event'],
-                                records[ID]['gender'],records[ID]['age'],
-                                records[ID]['name']]]
-
-    timeList.sort()                             # sorts the list.
-
-    return timeList
-
-
-## extractGender(timeList) splits the list generated by sortByList into two different lists based on gender.
-def extractGender(timeList):
-    male = []                                   # creates the two lists: one for males another for females.
-    female = []
-    for i in range(len(timeList)):              # split sequence.
-        if timeList[i][2] == 'F':
-            female = female + [timeList[i]]
-        else:
-            male = male + [timeList[i]]
-    return male,female                          # return both lists.
-
-
-## extractEvent(timeList): splits the cases stored in a list of times by event. This is an auxiliary function
-##                         for the function printByGender(gender).
-def extractEvent(timeList):
-    IM = []                                     # creates one list for each event.
-    BS = []
-    BA = []
-    CR = []
-    FS = []
-    for i in range(len(timeList)):              # distributes the cases into the right event.
-        if timeList[i][1] == 'IM':
-            IM = IM + [timeList[i]]
-        elif timeList[i][1] == 'BS':
-            BS = BS + [timeList[i]]
-        elif timeList[i][1] == 'BA':
-            BA = BA + [timeList[i]]
-        elif timeList[i][1] == 'CR':
-            CR = CR + [timeList[i]]
-        elif timeList[i][1] == 'FS':
-            FS = FS + [timeList[i]]
-            
-    return IM,BS,BA,CR,FS                       # returns the lists with the results of all the events.
-
-
-## printByGender(gender): takes each list of times created by the function extractGender(timeList) and print the
-##                        results. It is a subfunction of the main function printResults(records).
-def printByGender(gender):
-    IM,BS,BA,CR,FS = extractEvent(gender)           # asigns the records into the apropiate event list.
-
-    if debug == 1:
-        print len(IM),len(BS),len(BA),len(CR),len(FS)
-
-    if len(IM) != 0:                                # if there are records for an event, it prints the results,
-        print '\nTime\tEvent\tGender\tAge\tName'    
-        for i in range(len(IM)):
-            print IM[i][0],'\t',IM[i][1],'\t',IM[i][2],'\t',IM[i][3],'\t',IM[i][4]
-    else:
-        del IM                                      # otherwise, it deletes the list.
-    if len(BS) != 0:
-        print '\ntime\tEvent\tGender\tAge\tName'
-        for i in range(len(BS)):
-            print BS[i][0],'\t',BS[i][1],'\t',BS[i][2],'\t',BS[i][3],'\t',BS[i][4]
-    else:
-        del BS
-    if len(BA) != 0:
-        print '\ntime\tEvent\tGender\tAge\tName'
-        for i in range(len(BA)):
-            print BA[i][0],'\t',BA[i][1],'\t',BA[i][2],'\t',BA[i][3],'\t',BA[i][4]
-    else:
-        del BA
-    if len(CR) != 0:
-        print '\ntime\tEvent\tGender\tAge\tName'
-        for i in range(len(CR)):
-            print CR[i][0],'\t',CR[i][1],'\t',CR[i][2],'\t',CR[i][3],'\t',CR[i][4]
-    else:
-        del CR
-    if len(FS) != 0:
-        print '\ntime\tEvent\tGender\tAge\tName'
-        for i in range(len(FS)):
-            print FS[i][0],'\t',FS[i][1],'\t',FS[i][2],'\t',FS[i][3],'\t',FS[i][4]
-    else:
-        del FS
-        
-
 ## printResults(records): make use of the functions defined earlier to get an output of the
 ##                        results in the required the format.
 def printResults(records):
@@ -370,91 +197,7 @@ def printResults(records):
     male,female = extractGender(timeList)
     printByGender(female)
     printByGender(male)
-
-
-###############################################################################################################################
-###############################################################################################################################
-##
-## Data manipulation functions
-##
-###############################################################################################################################
-###############################################################################################################################
-##
-## chooseEvent(): is an auxiliary function for restricting the user's event input and keep the database
-##                integrity when updating a swimmer's personal best.
-def chooseEvent():
-    options = ('1','2','3','4','5')         # set of valid user's input options.
-    userInput = ''
-    print '\n1. Individual Medley (IM).'
-    print '2. Breast Stroke (BS).'
-    print '3. Backstroke (BA).'
-    print '4. Crawl (CR).'
-    print '5. Free Style (FS).'
-    print '\nChoose an option (1-5):',
-
-    while userInput not in options:         # user's input gather code and input exceptions handler.
-        userInput = raw_input()
-        if userInput not in options:
-            print '\nPlease type a number from 1 to 5:',
-
-    if userInput == '1':
-        event = 'IM'
-    elif userInput == '2':
-        event = 'BS'
-    elif userInput == '3':
-        event = 'BA'
-    elif userInput == '4':
-        event = 'CR'
-    elif userInput == '5':
-        event = 'FS'
-
-    return event                            # returns the event.
-
-
-## updateTimes(swimmer,temp,recors): this sub-function of the checkTimes(records) function is the actual bit
-##                                   of code that checks whether a swimmer's time is better than the one stored
-##                                   and updates the database. It takes four arguments: swimmer's name, a temporary
-##                                   dictionary with the swimmer's name as a key, the records dictionary, and the
-##                                   ID key of the swimmer in the main dictionary.
-def updateTimes(swimmer,temp,records,swimmerID):
-    print 'Please type time (format SS.D)',
-    time = input()
-    if time < temp[swimmer]['time']:        # checks if the new time is better.
-        event = chooseEvent()               # assigns the event where the new personal best has been achieved.
-        temp[swimmer]['time'] = time
-        temp[swimmer]['event'] = event
-        records[swimmerID] = temp[swimmer]  # updates the main dictionary.
-        print '\nPersonal best for',swimmer,'has been updated.'
-                
-    return records
-
-
-## checkTimes(records): this function goes throught the dictionary extract the apropiate case and updates the
-##                      personal best for the said swimmer.
-def checkTimes(records):
-    temp = {}                               # creates a temporary dictionary to store the swimmer's record.
-    swimmer = raw_input("\nPlease type swimmer's name: ")
-    for ID in records:                      # extracts swimmer from the database and place it into.
-        if records[ID]['name'] == swimmer:  # the temporary dictionary.
-            temp[swimmer] = records[ID]
-            swimmerID = ID                  # stores swimmer's key from the main dictionary.
-    
-    check = temp.get(swimmer,'not found')   # checks whether swimmer is in the database or not.
-
-    if debug == 1:
-        print temp
-        print check
-    
-    if check != 'not found':                # update routine
-        updateTimes(swimmer,temp,records,swimmerID)
-    else:
-        print swimmer,'not found',
-        retry = another()                   # retry routine if swimmer is not in the database.
-        if retry == 'y':
-            checkTimes(records)
-
-    return records
-
+	
 
 ###############################################################################################################################
 ###############################################################################################################################
@@ -486,15 +229,27 @@ def menu():
             print 'Please type a valid option (L/S/P/C/Q; no case-sensitive):',
 
     if userInput == 'l':                    # this bit of code fires the right function according to the user's choice.
+		print "Please enter which time of file you are loading, 0 for student records, 1 for courses"
+		fileType = input()
         #records = loadFile(0)
-        courses = loadFile(1)
+        #courses = loadFile(1)
+		if fileType == 0:
+			records = loadFile(0)
+		else
+			courses = loadFile(1)
     elif userInput == 's':
+		print "Please enter which time of file you want to save, 0 for student records, 1 for courses"
+		fileType = input()
+        #records = loadFile(0)
+        #courses = loadFile(1)
+		if fileType == 0:
+			saveFile(0, records)
+		else
+			saveFile(1, courses)
         #saveFile(0,records)
-        saveFile(1,courses)
+        #saveFile(1,courses)
     elif userInput == 'p':
         printResults(records)
-    elif userInput == 'c':
-        checkTimes(records)
     elif userInput == 'q':
         quitProgram = True
 
